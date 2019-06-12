3d7842e439171ef162784f0c461a4467d97fc005
Mike Lofgren <mikeylofgren@gmail.com>
2019-06-11 22:10:30 -0700

    Added a branch statement to the front end side.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
 var leapYear = function(year) {
-  if (year % 100 === 0) {
-    return false;
-    //if the year is divisble by 100 then it is not a leap year which is spit back to the user
-  } else if (year % 4 === 0) {
-    return true;
-    //if the year is divisible by 4 then it is a leap year and returns true to the user
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0 )) {
+    return true
+    //if the year is divisble by 4 or 400 but not 100 then it is not a leap year which is spit back to the user
   } else {
     return false;
   }
 };
+
 
 
 $(document).ready(function() {
@@ -20,8 +18,16 @@ $(document).ready(function() {
     //this variable year takes the input for input$years value with .val and parses it to make sure that it is an integer
     var result = leapYear(year);
     //This will call leapYear function, passing in the variable year as an
-    console.log(result)
-    $("#result").text(result);
-    //This displays the returned boolean value
+    $(".year").text(year);
+    //Add contents of the year variable into a span with the class year
+    if (!result) {
+      $(".not").text("not
+    //If the variable is false we set the text of the not span to not so the page reads not a leap year
+    } else {
+      $(".not").text("");
+    }
+    //If the variable is true instead empty the not span so it reads "is a leap year"
+    $("#result").show();
+    //Show entire result div so that the user can see their results
   });
 });
